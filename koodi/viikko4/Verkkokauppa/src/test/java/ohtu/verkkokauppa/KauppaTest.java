c18090135afeab272e277ec266becc562d8f8121
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-11-20 16:43:37 +0200

    Set up all the tests with mock classes and delete comments

--- koodi/viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ koodi/viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -6,42 +6,37 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class KauppaTest {
+    Pankki pankki;
+    Viitegeneraattori viite;
+    Varasto varasto;
+
+    @Before
+    public void setUp() {
+        pankki = mock(Pankki.class);
+        viite = mock(Viitegeneraattori.class);
+        varasto = mock(Varasto.class);
+    }
 
     @Test
     public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
-        // luodaan ensin mock-oliot
-        Pankki pankki = mock(Pankki.class);
-
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
-        // määritellään että viitegeneraattori palauttaa viitten 42
         when(viite.uusi()).thenReturn(42);
 
-        Varasto varasto = mock(Varasto.class);
-        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
 
-        // sitten testattava kauppa
         Kauppa k = new Kauppa(varasto, pankki, viite);
 
-        // tehdään ostokset
         k.aloitaAsiointi();
         k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
         k.tilimaksu("pekka", "12345");
 
-        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
         verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(),anyInt());
-        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
     }
 
     @Test
     public void afterPurchasePankkiMethodTilisiirtoGetsCalledWithRightParameters() {
-        Pankki pankki = mock(Pankki.class);
-
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
         when(viite.uusi()).thenReturn(42);
 
-        Varasto varasto = mock(Varasto.class);
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
 
@@ -56,12 +51,8 @@ public class KauppaTest {
 
     @Test
     public void afterTwoPurchasesPankkiMethodTilisiirtoGetsCalledWithRightParameters() {
-        Pankki pankki = mock(Pankki.class);
-
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
         when(viite.uusi()).thenReturn(42);
 
-        Varasto varasto = mock(Varasto.class);
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
         when(varasto.saldo(2)).thenReturn(10);
