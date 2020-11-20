46600adaf33f96cb00e944f4e50cf3d3ceda66cc
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-11-20 15:14:55 +0200

    Add new test to check that tilisiirto gets called with the right parameters

--- koodi/viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ koodi/viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -33,4 +33,24 @@ public class KauppaTest {
         verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(),anyInt());
         // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
     }
+
+    @Test
+    public void afterPurchasePankkiMethodTilisiirtoGetsCalledWithRightParameters() {
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        when(viite.uusi()).thenReturn(42);
+
+        Varasto varasto = mock(Varasto.class);
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 5);
+    }
 }
