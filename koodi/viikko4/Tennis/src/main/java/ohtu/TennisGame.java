ea6b8d15012710bb424034a2d9a5faab8ce5f3ea
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-11-23 20:49:24 +0200

    Refactor code

--- koodi/viikko4/Tennis/src/main/java/ohtu/TennisGame.java
+++ koodi/viikko4/Tennis/src/main/java/ohtu/TennisGame.java
@@ -2,10 +2,11 @@ package ohtu;
 
 public class TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int player1Score = 0;
+    private int player2Score = 0;
     private String player1Name;
     private String player2Name;
+    private static int GAME = 4;
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -13,68 +14,64 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName == player1Name)
+            player1Score += 1;
+        else if (playerName == player2Name) {
+            player2Score += 1;
+        }
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
+
+        if (player1Score == player2Score) {
+            return scoreResult(player2Score, player1Score);
+        }
+
+        int minScore = Math.min(player1Score, player2Score);
+        int maxScore = Math.max(player1Score, player2Score);
+        if (maxScore >= GAME) {
+            String leadingPlayer = player1Score < player2Score ? player2Name : player1Name;
+            switch(maxScore - minScore) {
                 case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
+                    score = "Advantage " + leadingPlayer;
                     break;
                 default:
-                        score = "Deuce";
-                    break;
-                
+                    score = "Win for " + leadingPlayer;
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        else {
+            return scoreResult(player2Score, player1Score);
         }
         return score;
     }
+
+    public static String resultName(int score) {
+        switch(score) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return "Deuce";
+        }
+    }
+
+    public static String scoreResult(int scoreSecondPlayer, int scoreFirstPlayer) {
+        String result = resultName(scoreFirstPlayer);
+        if (scoreSecondPlayer == scoreFirstPlayer && scoreSecondPlayer < GAME) {
+            return result + "-All";
+        }
+        else if (scoreSecondPlayer < GAME) {
+            return result + "-" + resultName(scoreSecondPlayer);
+        }
+        else {
+            return result;
+        }
+    }
+
 }
