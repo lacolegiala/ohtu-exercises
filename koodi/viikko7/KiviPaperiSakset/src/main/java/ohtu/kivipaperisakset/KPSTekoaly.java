1ba488d99d0a2a7df9f81fe34dfcd8b52af191da
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-12-14 19:37:56 +0200

    Make tekoäly private

--- koodi/viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSTekoaly.java
+++ koodi/viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSTekoaly.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class KPSTekoaly extends RockPaperScissors {
 
     private static final Scanner scanner = new Scanner(System.in);
-    Tekoaly tekoaly = new Tekoaly();
+    private Tekoaly tekoaly = new Tekoaly();
 
     @Override
     protected String toisenSiirto(String firstMove) {
