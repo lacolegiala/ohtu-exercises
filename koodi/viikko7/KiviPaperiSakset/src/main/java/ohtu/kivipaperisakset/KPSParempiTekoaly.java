875f9637a72c2127c69c5e1fd4c40d4a1a8dc3c6
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-12-14 19:37:28 +0200

    Refactor parempi tekoäly

--- koodi/viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSParempiTekoaly.java
+++ koodi/viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSParempiTekoaly.java
@@ -6,43 +6,17 @@ import java.util.Scanner;
 
 // Kivi-Paperi-Sakset, jossa voidaan valita pelataanko vastustajaa
 // vastaan vai ei
-public class KPSParempiTekoaly {
+public class KPSParempiTekoaly extends RockPaperScissors {
 
     private static final Scanner scanner = new Scanner(System.in);
-
-    public void pelaa() {
-        Tuomari tuomari = new Tuomari();
-        TekoalyParannettu tekoaly = new TekoalyParannettu(20);
-
-        System.out.println("Ensimmäisen pelaajan siirto: ");
-        String ekanSiirto = scanner.nextLine();
-        String tokanSiirto;
-
-
-        tokanSiirto = tekoaly.annaSiirto();
-        System.out.println("Tietokone valitsi: " + tokanSiirto);
-
-
-        while (onkoOkSiirto(ekanSiirto) && onkoOkSiirto(tokanSiirto)) {
-            tuomari.kirjaaSiirto(ekanSiirto, tokanSiirto);
-            System.out.println(tuomari);
-            System.out.println();
-
-            System.out.println("Ensimmäisen pelaajan siirto: ");
-            ekanSiirto = scanner.nextLine();
-
-            tokanSiirto = tekoaly.annaSiirto();
-            System.out.println("Tietokone valitsi: " + tokanSiirto);
-            tekoaly.asetaSiirto(ekanSiirto);
-
-        }
-
-        System.out.println();
-        System.out.println("Kiitos!");
-        System.out.println(tuomari);
+    private TekoalyParannettu tekoaly = new TekoalyParannettu(20);
+    
+    @Override
+    protected String toisenSiirto(String firstMove) {
+        String secondMove = tekoaly.annaSiirto();
+        System.out.println("Tietokone valitsi: " + secondMove);
+        tekoaly.asetaSiirto(firstMove);
+        return secondMove;
     }
 
-    private static boolean onkoOkSiirto(String siirto) {
-        return "k".equals(siirto) || "p".equals(siirto) || "s".equals(siirto);
-    }
 }
